687fb4d922b468c674a11e7e65c65eb80cb32be6
David Rosinski <drosinski@outlook.com>
2018-04-24 21:49:06 -0500

    Added delete functionality

--- app/src/main/java/com/example/drosi/vehicle_manager/ExpandableListViewAdapter.java
+++ app/src/main/java/com/example/drosi/vehicle_manager/ExpandableListViewAdapter.java
@@ -1,16 +1,22 @@
 package com.example.drosi.vehicle_manager;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.media.Image;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +25,9 @@ public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
 
     // The arraylists to store the vehicle information
     public ArrayList<String> groupNames = new ArrayList<String>();
+    private String[] dbGroupIds;    // Stores the database IDs so the proper record is deleted
     public List<List<String>> childNames = new ArrayList<List<String>>();
+    public ArrayList<ImageView> deleteButtons = new ArrayList<ImageView>();
 
     //Context context;
     private Activity context;
@@ -32,19 +40,29 @@ public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
 
         Cursor data = mDatabaseHelper.getData("SELECT * FROM " + mDatabaseHelper.TABLE_NAME);
 
+        // Initialize the visibility array with values showing the Xs are invisible
+        int groupCount = data.getCount();
+        ImageView spaceHolder = new ImageView(context); // Create spaceholders for the Xs arraylist
+
+        for (int i = 0; i < groupCount; i++) {
+            deleteButtons.add(spaceHolder);
+        }
+        dbGroupIds = new String[groupCount];
         while(data.moveToNext()) {
             // The database has 4 columns labeled as such...
-            // ID - column 0
+            // ID - column 0 - KEY
             // VIN - Column 1
             // YEAR - Column 2
             // MAKE - Column 3
             // MODEL - Column 4
             // DESC - Column 5
 
-
             // Add the name as the groupname
             String name = data.getString(2) + " " + data.getString(3) + " " + data.getString(4);
             groupNames.add(name);
+
+            // Set all the database IDs equal to the position of the vehicles in the listview
+            dbGroupIds[data.getPosition()] = data.getString(0);
 
             // Add the vin and description as child elements
             List<String> groupChildren = new ArrayList<String>();
@@ -90,8 +108,8 @@ public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
     }
 
     @Override
-    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        String groupName = groupNames.get(groupPosition);
+    public View getGroupView(final int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        final String groupName = groupNames.get(groupPosition);
 
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -100,6 +118,51 @@ public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
         TextView groupItem = (TextView) convertView.findViewById(R.id.groupName);
         groupItem.setTypeface(null, Typeface.BOLD);
         groupItem.setText(groupName);
+
+        // Create the red X for deleting vehicles
+        final ImageView deleteImage = (ImageView) convertView.findViewById(R.id.group_delete);
+        deleteButtons.set(groupPosition, deleteImage);
+
+        // Add the click listener for the deleting x
+        deleteImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a dialog box asking the user to confirm
+                // First create the dialog click listener
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case DialogInterface.BUTTON_POSITIVE:
+                                // The user confirmed yes, they're sure
+                                deleteVehicle(groupPosition);
+
+                                // Remove the delete Xs
+                                for (ImageView delete : deleteButtons) {
+                                    if (delete.getVisibility() == View.INVISIBLE) {
+                                        delete.setVisibility(View.VISIBLE);
+                                    } else if (delete.getVisibility() == View.VISIBLE) {
+                                        delete.setVisibility(View.INVISIBLE);
+                                    }
+                                }
+                                groupNames.remove(groupPosition);
+                                notifyDataSetChanged();
+                                break;
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                // The user canceled or said no
+                                break;
+                        }
+                    }
+                };
+
+                // Now that the dialog listener is created, create the actual dialog box
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setMessage("Are you sure you want to delete " + groupName + "?")
+                        .setPositiveButton("Yes", dialogClickListener)
+                        .setNegativeButton("No", dialogClickListener).show();
+            }
+        });
+
         return convertView;
     }
 
@@ -122,5 +185,25 @@ public class ExpandableListViewAdapter extends BaseExpandableListAdapter {
     @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
         return false;
+    }
+
+    public ArrayList<ImageView> getDeleteImages() {
+        return deleteButtons;
+    }
+
+    private boolean deleteVehicle(int groupID) {
+        // Load the database
+        DatabaseHelper mDatabaseHelper = new DatabaseHelper(context);
+        SQLiteDatabase db = mDatabaseHelper.getWritableDatabase();
+
+        String whereClause = "id=?";
+        String[] whereArgs = new String[] {dbGroupIds[groupID]};
+        int delData = db.delete(mDatabaseHelper.TABLE_NAME, whereClause, whereArgs);
+        if (delData > 0) {
+            // Delete operation was a success!
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- app/src/main/java/com/example/drosi/vehicle_manager/MainActivity.java
+++ app/src/main/java/com/example/drosi/vehicle_manager/MainActivity.java
@@ -8,15 +8,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    ExpandableListView vehicleList;
+    private Boolean isGroupCollapsed;
+    private ExpandableListView vehicleList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create the expandable list of vehicles
+        vehicleList = (ExpandableListView)findViewById(R.id.vehicleList);
+        final ExpandableListViewAdapter adapter = new ExpandableListViewAdapter(MainActivity.this);
+        vehicleList.setAdapter(adapter);
 
         // Setup the fab to add a vehicle
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabAdd);
@@ -28,9 +37,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Create the expandable list of vehicles
-        vehicleList = (ExpandableListView)findViewById(R.id.vehicleList);
-        ExpandableListViewAdapter adapter = new ExpandableListViewAdapter(MainActivity.this);
-        vehicleList.setAdapter(adapter);
+        // Setup fab to remove vehicles from the list
+        FloatingActionButton fabRemove = (FloatingActionButton) findViewById(R.id.fabRemove);
+        fabRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Make the red Xs visible on the list
+                ArrayList<ImageView> deleteButtons = adapter.getDeleteImages();
+
+                // Collapse the groups if they're opened
+                for (int i = 0; i < adapter.getGroupCount(); i++) {
+                        vehicleList.collapseGroup(i);
+                }
+
+                for (ImageView delete : deleteButtons) {
+                    if (delete.getVisibility() == View.INVISIBLE) {
+                        delete.setVisibility(View.VISIBLE);
+                    } else if (delete.getVisibility() == View.VISIBLE) {
+                        delete.setVisibility(View.INVISIBLE);
+                    }
+                }
+            }
+        });
     }
 }
